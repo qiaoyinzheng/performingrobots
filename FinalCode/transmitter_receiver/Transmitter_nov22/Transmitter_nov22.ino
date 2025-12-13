27f13c5b6db434d859306052e12d767355862827
Joy Zheng <112919242+qiaoyinzheng@users.noreply.github.com>
2025-12-13 18:01:33 +0400

    Final Code After Show
    For future robot people:
    1. The RC sometimes sent opposite signals for forward and reverse, so we prepared 2 codes for both situations.
    2. The /* comment didn't work well with our long color code, so we cannot follow Prof's instruction to include in the same file.

---
+++ FinalCode/transmitter_receiver/Transmitter_nov22/Transmitter_nov22.ino
@@ -0,0 +1,355 @@
+/*
+   Using the nRF24L01 radio module to communicate
+   between two Arduinos with much increased reliability following
+   various tutorials, conversations, and studying the nRF24L01 datasheet
+   and the library reference.
+
+   Transmitter is
+   https://github.com/michaelshiloh/resourcesForClasses/tree/master/kicad/Arduino_Shield_RC_Controller
+
+  Receiver is
+  https://github.com/michaelshiloh/resourcesForClasses/blob/master/kicad/nRF_servo_Mega
+
+   This file contains code for both transmitter and receiver.
+   Transmitter at the top, receiver at the bottom.
+   One of them is commented out, so you need to comment in or out
+   the correct section. You don't need to make changes to this 
+   part of the code, just to comment in or out depending on
+   whether you are programming your transmitter or receiver
+
+   You need to set the correct address for your robot.
+
+   Search for the phrase CHANGEHERE to see where to 
+   comment or uncomment or make changes.
+
+   These sketches require the RF24 library by TMRh20
+   Documentation here: https://nrf24.github.io/RF24/index.html
+
+   change log
+
+   11 Oct 2023 - ms - initial entry based on
+                  rf24PerformingRobotsTemplate
+   26 Oct 2023 - ms - revised for new board: nRF_Servo_Mega rev 2
+   28 Oct 2023 - ms - add demo of NeoMatrix, servo, and Music Maker Shield
+	 20 Nov 2023 - as - fixed the bug which allowed counting beyond the limits
+   22 Nov 2023 - ms - display radio custom address byte and channel
+   12 Nov 2024 - ms - changed names for channel and address allocation for Fall 2024            
+   31 Oct 2025 - ms - changed names for channel and address allocation for Fall 2024            
+                    - listed pin numbers for servo/NeoPixel connections
+                      https://github.com/michaelshiloh/resourcesForClasses/blob/master/kicad/nRF_servo_Mega    
+                      https://github.com/michaelshiloh/resourcesForClasses/blob/master/kicad/nRFControlPanel
+*/
+
+
+// Common code
+//
+
+// Common pin usage
+// Note there are additional pins unique to transmitter or receiver
+//
+
+// nRF24L01 uses SPI which is fixed
+// on pins 11, 12, and 13 on the Uno
+// and on pins 50, 51, and 52 on the Mega
+
+// It also requires two other signals
+// (CE = Chip Enable, CSN = Chip Select Not)
+// Which can be any pins:
+
+// CHANGEHERE
+// For the transmitter
+const int NRF_CE_PIN = A4, NRF_CSN_PIN = A5;
+
+// CHANGEHERE
+// for the receiver
+// const int NRF_CE_PIN = A11, NRF_CSN_PIN = A15;
+
+// nRF 24L01 pin   name
+//          1      GND
+//          2      3.3V
+//          3      CE
+//          4      CSN
+//          5      SCLK
+//          6      MOSI/COPI
+//          7      MISO/CIPO
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(NRF_CE_PIN, NRF_CSN_PIN);  // CE, CSN
+
+//#include <printf.h>  // for debugging
+
+// See note in rf24Handshaking about address selection
+//
+
+// Channel and address allocation:
+// Torico and Sarah: Channel 30, addr = 0x76
+// Sudiksha and Aysha: Channel 40, addr = 0x73
+// Mariam and Joy:  Channel 50, addr = 0x7C
+// Ghadir and Mustafa: Channel 60, addr = 0xC6
+// Clara and Jiho:  Channel 70, addr = 0xC3
+// Victor and Meera: Channel 80, addr = 0xCC
+// Ali and Hari: Channel 90, addr = 0x33
+
+// CHANGEHERE
+const int CUSTOM_CHANNEL_NUMBER = 50;   // change as per the above assignment
+const byte CUSTOM_ADDRESS_BYTE = 0x7C;  // change as per the above assignment
+
+// Do not make changes here
+const byte xmtrAddress[] = { CUSTOM_ADDRESS_BYTE, CUSTOM_ADDRESS_BYTE, 0xC7, 0xE6, 0xCC };
+const byte rcvrAddress[] = { CUSTOM_ADDRESS_BYTE, CUSTOM_ADDRESS_BYTE, 0xC7, 0xE6, 0x66 };
+
+const int RF24_POWER_LEVEL = RF24_PA_LOW;
+
+// global variables
+uint8_t pipeNum;
+unsigned int totalTransmitFailures = 0;
+
+struct DataStruct {
+  uint8_t stateNumber;
+};
+DataStruct data;
+
+void setupRF24Common() {
+
+  // RF24 setup
+  if (!radio.begin()) {
+    Serial.println(F("radio  initialization failed"));
+    while (1)
+      ;
+  } else {
+    Serial.println(F("radio successfully initialized"));
+  }
+
+  radio.setDataRate(RF24_250KBPS);
+  radio.setChannel(CUSTOM_CHANNEL_NUMBER);
+  radio.setPALevel(RF24_POWER_LEVEL);
+}
+
+// CHANGEHERE
+
+
+// Start of Transmitter code
+// Choose Arduino Uno port
+
+// Transmitter pin usage
+const int LCD_RS_PIN = 3, LCD_EN_PIN = 2, LCD_D4_PIN = 4, LCD_D5_PIN = 5, LCD_D6_PIN = 6, LCD_D7_PIN = 7;
+const int SW1_PIN = 8, SW2_PIN = 9, SW3_PIN = 10, SW4_PIN = A3, SW5_PIN = A2;
+
+// LCD library code
+#include <LiquidCrystal.h>
+
+// initialize the library with the relevant pins
+LiquidCrystal lcd(LCD_RS_PIN, LCD_EN_PIN, LCD_D4_PIN, LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
+
+
+const int NUM_OF_STATES = 41;
+const char* theStates[] = {
+  "0 backstage mode",
+  "1 dance mode",
+  "2 dance to normal mode",
+  "3 they always are",
+  "4 dance to normal mode",
+  "5 close face black out",
+  "6 face on light on",
+  "7 so disco night",
+  "8 feeling fly",
+  "9 overreacting",
+  "10 you're supposed",
+  "11 Ohhhhh here ",
+  "12 knife mode angry face",
+  "13 go to bed",
+  "14 put down mode",
+  "15 you know what",
+  "16 see if",
+  "17 knife mode",
+  "18 filing for",
+  "19 put down",
+  "20 black out face",
+  "21 face light on",
+  "22 of course",
+  "23 your coffee",
+  "24 and gain a",
+  "25 black out face",
+  "26 light on face",
+  "27 and saul don't",
+  "28 we were",
+  "29 group gasp",
+  "30 honestly",
+  "31 we discussed this",
+  "32 group gasp",
+  "33 this isn't",
+  "34 she understood",
+  "35 angry face",
+  "36 normal face",
+  "37 dance move",
+  "38 close face",
+  "39 knife mode",
+  "40 memory print"
+};
+
+
+void updateLCD() {
+
+  lcd.clear();
+  lcd.print(theStates[data.stateNumber]);
+  lcd.setCursor(0, 1);  // column, line (from 0)
+  lcd.print("not transmitted yet");
+}
+
+void countDown() {
+  data.stateNumber = (data.stateNumber > 0) ? (data.stateNumber - 1) : 0;
+  updateLCD();
+}
+
+void countUp() {
+  if (++data.stateNumber >= NUM_OF_STATES) {
+    data.stateNumber = NUM_OF_STATES - 1;
+  }
+  updateLCD();
+}
+
+
+void spare1() {}
+void spare2() {}
+
+void rf24SendData() {
+
+  radio.stopListening();  // go into transmit mode
+  // The write() function will block
+  // until the message is successfully acknowledged by the receiver
+  // or the timeout/retransmit maxima are reached.
+  // Returns 1 if write succeeds
+  // Returns 0 if errors occurred (timeout or FAILURE_HANDLING fails)
+  int retval = radio.write(&data, sizeof(data));
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);  // column, line (from 0)
+  lcd.print("transmitting");
+  lcd.setCursor(14, 0);  // column, line (from 0)
+  lcd.print(data.stateNumber);
+
+  Serial.print(F(" ... "));
+  if (retval) {
+    Serial.println(F("success"));
+    lcd.setCursor(0, 1);  // column, line (from 0)
+    lcd.print("success");
+  } else {
+    totalTransmitFailures++;
+    Serial.print(F("failure, total failures = "));
+    Serial.println(totalTransmitFailures);
+
+    lcd.setCursor(0, 1);  // column, line (from 0)
+    lcd.print("error, total=");
+    lcd.setCursor(13, 1);  // column, line (from 0)
+    lcd.print(totalTransmitFailures);
+  }
+}
+
+class Button {
+  int pinNumber;
+  bool previousState;
+  void (*buttonFunction)();
+public:
+
+  // Constructor
+  Button(int pn, void* bf) {
+    pinNumber = pn;
+    buttonFunction = bf;
+    previousState = 1;
+  }
+
+  // update the button
+  void update() {
+    bool currentState = digitalRead(pinNumber);
+    if (currentState == LOW && previousState == HIGH) {
+      Serial.print("button on pin ");
+      Serial.print(pinNumber);
+      Serial.println();
+      buttonFunction();
+    }
+    previousState = currentState;
+  }
+};
+
+const int NUMBUTTONS = 5;
+Button theButtons[] = {
+  Button(SW1_PIN, countDown),
+  Button(SW2_PIN, rf24SendData),
+  Button(SW3_PIN, countUp),
+  Button(SW4_PIN, spare1),
+  Button(SW5_PIN, spare2),
+};
+
+void setupRF24() {
+
+  setupRF24Common();
+
+  // Set us as a transmitter
+  radio.openWritingPipe(xmtrAddress);
+  radio.openReadingPipe(1, rcvrAddress);
+
+  // radio.printPrettyDetails();
+  Serial.println(F("I am a transmitter"));
+
+  data.stateNumber = 0;
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("Setting up LCD"));
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  lcd.clear();
+  // Print a message to the LCD.
+  lcd.print("Radio setup");
+
+  // Display the address in hex
+  lcd.setCursor(0, 1);
+  lcd.print("addr 0x");
+  lcd.setCursor(7, 1);
+  char s[5];
+  sprintf(s, "%02x", CUSTOM_ADDRESS_BYTE);
+  lcd.print(s);
+
+  // Display the channel number
+  lcd.setCursor(10, 1);
+  lcd.print("ch");
+  lcd.setCursor(13, 1);
+  lcd.print(CUSTOM_CHANNEL_NUMBER);
+
+  Serial.println(F("Setting up radio"));
+  setupRF24();
+
+  // If setupRF24 returned then the radio is set up
+  lcd.setCursor(0, 0);
+  lcd.print("Radio OK state=");
+  lcd.print(theStates[data.stateNumber]);
+
+  // Initialize the switches
+  pinMode(SW1_PIN, INPUT_PULLUP);
+  pinMode(SW2_PIN, INPUT_PULLUP);
+  pinMode(SW3_PIN, INPUT_PULLUP);
+  pinMode(SW4_PIN, INPUT_PULLUP);
+  pinMode(SW5_PIN, INPUT_PULLUP);
+}
+
+
+
+void loop() {
+  for (int i = 0; i < NUMBUTTONS; i++) {
+    theButtons[i].update();
+  }
+  delay(50);  // for testing
+}
+
+
+void clearData() {
+  // set all fields to 0
+  data.stateNumber = 0;
+}
+
+// End of transmitter code
+// CHANGEHERE
